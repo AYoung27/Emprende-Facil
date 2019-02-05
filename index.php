54cc3c8de5d1aefa0e69dc0bd265d623aa16b2d5
abn1227 <37668034+abn1227@users.noreply.github.com>
2019-02-05 16:50:31 -0600

    Actualizacion
    Actualizacion de index.php
    Se eliminaron los archivos HTML

--- index.php
+++ index.php
@@ -4,21 +4,34 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="author" description="Abner">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 	<link rel="icon" type="png" href="img/png/041-online-shop.png">
 	<link rel="stylesheet" type="text/css" href="Estilos/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="Estilos/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="Estilos/css/style.css">
+    <link href="Estilos/css/carousel.css" rel="stylesheet">
+	<title>Emprende Fácil</title>
+	<style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+      }
 
-	<title>EmprendeFacil</title>
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
 </head>
 <body>
 	<header>
 		<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-custom">	 
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 				<span class="navbar-toggler-icon"></span>
-			</button> 
+			</button>
 			<a class="navbar-brand" href="#">
-				Emprende Facil
+				Emprende Fácil
 			</a>
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="navbar-nav mr-auto">
@@ -26,86 +39,158 @@
 						<a class="nav-link" href="registro.php">Registrarse<span class="sr-only">(current)</span></a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="login.php">Iniciar Sesion</a>
+						<a class="nav-link" href="login.php">Iniciar Sesión</a>
 					</li>
 				</ul>
-			</div>
-			<form class="form-inline mt-2 mt-md-0">
-				<input class="form-control mr-sm-2" type="text" placeholder="Buscar un producto" size="85 auto" /> 
+				<form class="form-inline mt-2 mt-md-0">
+				<input class="form-control mr-sm-2" type="search" placeholder="Buscar un producto" size="60" /> 
 				<button class="btn btn-primary my-2 my-sm-0" type="submit">
 					<span class="glyphicon glyphicon-search"></span>
 				</button>
+				<div class="dropdown" style="margin-left:auto; margin-right:0;">
+					<button class="btn" type="button" id="btnDropDown" data-toggle="dropdown" data-target = "#menDropDown" >
+						<span id="spnMenu" class="glyphicon glyphicon-bell" style="color: black"></span>
+					</button>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton" id="menDropDown">
+						<a class="dropdown-item" href="#">Notificación 1</a> 
+					 	<a class="dropdown-item" href="#">Notificación 2</a> 
+					 	<a class="dropdown-item" href="#">Notificación 3</a>
+					</div>
+				</div>
+				<div class="dropdown" style="margin-left:auto; margin-right:0;">
+					<button class="btn dropdown-toggle ml-auto" type="button" id="btnDropDown_2" data-toggle="dropdown" data-target = "#menDropDown_2" style="float: right;">
+					</button>
+					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton" id="menDropDown_2">
+						<a class="dropdown-item" href="#">Perfil</a> 
+						<a class="dropdown-item" href="#">Otro</a> 
+						<a class="dropdown-item" href="#">Salir</a>
+					</div>
+				</div>
 			</form>
-
-			<ul>
-				<li> <img src="Campana_notificacion_1.png"></li>
-			</ul>
+			</div>
 		</nav>
 	</header>
+
 	<main role="main">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="carousel slide" id="carousel-618877">
-					<ol class="carousel-indicators">
-						<li data-slide-to="0" data-target="#carousel-618877" class="active">
-						</li>
-						<li data-slide-to="1" data-target="#carousel-618877">
-						</li>
-						<li data-slide-to="2" data-target="#carousel-618877">
-						</li>
-					</ol>
-					<div class="carousel-inner">
-						<div class="carousel-item active">
-							<img class="d-block w-100" alt="Carousel Bootstrap First" src="https://www.layoutit.com/img/sports-q-c-1600-500-1.jpg">
-							<div class="carousel-caption">
-								<h4>
-									Producto #1
-								</h4>
-								<p>
-									Parrafo publicitario
-								</p>
-							</div>
-						</div>
-						<div class="carousel-item">
-							<img class="d-block w-100" alt="Carousel Bootstrap Second" src="https://www.layoutit.com/img/sports-q-c-1600-500-2.jpg">
-							<div class="carousel-caption">
-								<h4>
-									Producto #2
-								</h4>
-								<p>
-									Parrafo publicitario
-								</p>
-							</div>
-						</div>
-						<div class="carousel-item">
-							<img class="d-block w-100" alt="Carousel Bootstrap Third" src="https://www.layoutit.com/img/sports-q-c-1600-500-3.jpg">
-							<div class="carousel-caption">
-								<h4>
-									Producto #3
-								</h4>
-								<p>
-									Parrafo publicitario
-								</p>
-							</div>
+		<div class="carousel slide " id="carousel-618877" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-slide-to="0" data-target="#carousel-618877" class="active">
+				</li>
+				<li data-slide-to="1" data-target="#carousel-618877">
+				</li>
+				<li data-slide-to="2" data-target="#carousel-618877">
+				</li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<svg width="100%" height="100%" preserveAspectRatio="xMidYMid slice" class="bd-placeholder-img" focusable="false" role="img">
+						<img src="img/1.jpeg" >
+					</svg>
+					<div class="container">
+						<div class="carousel-caption">
+							<h4>
+								Producto #1
+							</h4>
+							<p>
+								Párrafo publicitario
+							</p>
 						</div>
 					</div>
-					<a class="carousel-control-prev" href="#carousel-618877" data-slide="prev">
-						<span class="carousel-control-prev-icon"></span> 
-						<span class="sr-only">Previous</span>
-					</a> 
-					<a class="carousel-control-next" href="#carousel-618877" data-slide="next">
-						<span class="carousel-control-next-icon"></span> 
-						<span class="sr-only">Next</span>
-					</a>
+				</div>
+				<div class="carousel-item" >
+					<svg width="100%" height="100%" class="bd-placeholder-img" preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
+						<img src="img/2.png">
+					</svg>
+					<div class="container">
+						<div class="carousel-caption">
+							<h4>
+								Producto #2
+							</h4>
+							<p>
+								Párrafo publicitario
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class="carousel-item">
+					<svg width="100%" height="100%" class="bd-placeholder-img" focusable="false" role="img" preserveAspectRatio="xMidYMid slice">
+						<img src="img/3.jpg">
+					</svg>
+					<div class="container">
+						<div class="carousel-caption">
+							<h4>
+								Producto #3
+							</h4>
+							<p>
+								Párrafo publicitario
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
+			<a class="carousel-control-prev" href="#carousel-618877" data-slide="prev">
+				<span class="carousel-control-prev-icon"></span> 
+				<span class="sr-only">Previous</span>
+			</a> 
+			<a class="carousel-control-next" href="#carousel-618877" data-slide="next">
+				<span class="carousel-control-next-icon"></span> 
+				<span class="sr-only">Next</span>
+			</a>
 		</div>
+</div>
+
 		<div class="container marketing">
+			<div class="row">
+      			<div class="col-lg-4">
+      				<div class="card bg-default">
+						<h5 class="card-header">
+							Producto X
+						</h5>
+						<div class="card-body">
+							<img class="bd-placeholder-img rounded" width="140" height="140" src="img/png/006-shopping.png">
+        				</div>
+						<div class="card-footer">
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+        					<p>
+        					<a class="btn btn-secondary" href="#" role="button">Ver mas &raquo;</a></p>
+						</div>
+					</div>      			</div>
+      			<div class="col-lg-4">
+      				<div class="card bg-default">
+						<h5 class="card-header">
+							Producto Y
+						</h5>
+						<div class="card-body">
+							<img class="bd-placeholder-img rounded" width="140" height="140" src="img/png/006-shopping.png">
+        				</div>
+						<div class="card-footer">
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+        					<p>
+        					<a class="btn btn-secondary" href="#" role="button">Ver mas &raquo;</a></p>
+						</div>
+					</div>      			</div>
+      			<div class="col-lg-4">
+      				<div class="card bg-default">
+						<h5 class="card-header">
+							Producto Z
+						</h5>
+						<div class="card-body">
+							<img class="bd-placeholder-img rounded" width="140" height="140" src="img/png/006-shopping.png">
+        				</div>
+						<div class="card-footer">
+							<p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+        					<p>
+        					<a class="btn btn-secondary" href="#" role="button">Ver mas &raquo;</a></p>
+						</div>
+					</div>
+        		</div>    
+      		</div>
+
 			<hr class="featurette-divider">
 			<div class="row featurette">
 				<div class="col-md-7">
-					<h2 class="featurette-heading">Beneficios de nuestro sitio web. <span class="text-muted">Te sorprenderan.</span></h2>
-					<p class="lead">Parrafo.</p>
+					<h2 class="featurette-heading">Beneficios de nuestro sitio web. <span class="text-muted">Te sorprenderán.</span></h2>
+					<p class="lead">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 				</div>
 				<div class="col-md-5">
 				</div>
@@ -113,16 +198,27 @@
 			<hr class="featurette-divider">
 			<div class="row featurette">
 				<div class="col-md-7 order-md-2">
-					<h2 class="featurette-heading">Inscribete ya y<span class="text-muted"> publilcita tus productos.</span></h2>
-					<p class="lead">Parrafo</p>
+					<h2 class="featurette-heading">Inscríbete ya y<span class="text-muted"> publicita tus productos.</span></h2>
+					<p class="lead">Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
 				</div>
 				<div class="col-md-5 order-md-1">
 				</div>
 			</div>
 		</div>
-		<script src="Estilos/js/jquery.min.js"></script>
+		<p class="mt-5 mb-3 text-muted text-center">&copy; 2018-2019</p>
+	</main>
+	<script src="Estilos/js/jquery.min.js"></script>
 		<script src="Estilos/js/bootstrap.min.js"></script>
 		<script src="Estilos/js/scripts.js"></script>	
-	</main>
+		<script type="text/javascript">
+			function changeColor(x){
+				if(x.style.color=="black"){
+        			x.style.color="white";
+			    }else{
+			        x.style.color="black";
+			    }
+			    return false;
+			}
+		</script>
 </body>
 </html>
