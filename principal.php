8e0e795896965b811751cb1ae079d84a863371d0
AYoung27 <adan_19941@hotmail.com>
2019-02-04 19:02:17 -0600

    Formularios

---
+++ principal.php
@@ -0,0 +1,162 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+		<meta name="author" description="Abner">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="icon" type="png" href="img/png/041-online-shop.png">
+		<link rel="stylesheet" type="text/css" href="Estilos/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="Estilos/css/bootstrap.css">
+	<title>Emprende Facil</title>
+		<style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 500px;
+  }
+  </style>
+</head>
+<body>
+	<header>
+			<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-custom">	 
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="navbar-toggler-icon"></span>
+				</button> 
+				<a class="navbar-brand" href="#">
+					Emprende Facil
+				</a>
+				
+
+			</nav>
+	</header>
+
+	<div style="margin-top:50px">
+	  <ul class="nav nav-tabs">
+  		<li class="nav-item">
+  		  <a class="nav-link active" data-toggle="tab" href="#Inicio">Inicio</a>
+  		</li>
+  		<li class="nav-item">
+  		  <a class="nav-link" data-toggle="tab" href="#Busqueda">Busqueda de productos</a>
+  		</li>
+  		<li class="nav-item">
+  		  <a class="nav-link" data-toggle="tab" href="#Inventario">Inventario</a>
+  		</li>
+  		 <li class="nav-item">
+  		  <a class="nav-link" data-toggle="tab" href="#Clientes">Clientes</a>
+  		</li>
+  		 <li class="nav-item">
+  		  <a class="nav-link" data-toggle="tab" href="#Nosotros">Acerca de nosotros</a>
+  		</li>
+	  </ul>
+
+<!-- Tab panes -->
+<div class="tab-content">
+  <div class="tab-pane container active" id="Inicio">...</div>
+  <div class="tab-pane container fade" id="Busqueda">...</div>
+  <div class="tab-pane container fade" id="Inventario">
+  	<div>
+  		<h1 class="text-center" >Inventario</h1>
+    		  <div class="card text-center border-info">
+    		      <div class="card-header">
+    		        	<ul class="nav nav-tabs">
+  							<li class="nav-item">
+  							  <a class="nav-link active" data-toggle="tab" href="#Excistencia">Existencia</a>
+  							</li>
+  							<li class="nav-item">
+  							  <a class="nav-link" data-toggle="tab" href="#Ventas">Ventas</a>
+  							</li>
+  							<li class="nav-item">
+  							  <a class="nav-link" data-toggle="tab" href="#Pedidos">Pedidos</a>
+  							</li>
+ 							<li class="nav-item">
+    							<a class="nav-link" data-toggle="tab" href="#Estadisticas">Estadisticas</a>
+ 							</li>
+   							<li class="nav-item">
+   								 <a class="nav-link" data-toggle="tab" href="#Reportes">Reportes</a>
+ 							</li>
+						</ul>
+						<div class="tab-content">
+  							<div class="tab-pane container active" id="Existencia">...</div>
+  							<div class="tab-pane container fade" id="Ventas">...</div>
+  							<div class="tab-pane container fade" id="Pedidos">...</div>
+  							<div class="tab-pane container fade" id="Estadisticas">...</div>
+  							<div class="tab-pane container fade" id="Reportes">...</div>
+						</div>
+    		      </div>          
+    		  </div>
+  		</div>
+  </div>
+  <div class="tab-pane container fade" id="Clientes" >
+  		<div >
+  		<h1 class="text-center" >Clientes</h1>
+  		<form class="form-inline mt-2 mt-md-0">
+						<input class="form-control mr-sm-2" type="text" placeholder="Buscar " size="130 auto" /> 
+						<button class="btn btn-primary my-2 my-sm-0" type="submit">
+                            <span class="glyphicon glyphicon-search"></span>
+						</button>
+					</form>
+  			
+  		</div>
+  </div>
+  <div class="tab-pane container fade" id="Nosotros">
+  	<div>
+  		<h1 class="text-center">Acerca de nosotros</h1><br>
+  		<div class="text-center">
+  			<h3>Nuestra mision</h1>
+  			<article>
+				Poder ser el enlace entre proveedores y emprendedores para impulsar la economía y el 
+				crecimiento de pequeñas empresas ganando la confianza de cada ciudadano que utilice 
+				la plataforma, proporcionando así un servicio de excelencia y efectivo.
+  			</article><br>
+  			<h3>Nuestra vision</h3>
+  			<article>
+  				Poder llegar a nivel nacional y ser la aplicación más utilizada de los hondureños.
+			</article><br>
+		  <div id="demo" class="carousel slide text-center" data-ride="carousel">
+			  <ul class="carousel-indicators">
+			    <li data-target="#demo" data-slide-to="0" class="active"></li>
+			    <li data-target="#demo" data-slide-to="1"></li>
+			    <li data-target="#demo" data-slide-to="2"></li>
+			  </ul>
+			  <div class="carousel-inner">
+			    <div class="carousel-item active">
+			      <img src="img/1.jpeg" alt="Los Angeles" width="1100" height="500">
+			      <div class="carousel-caption">
+			        <h3>¡Emprender ahora es facil!</h3>
+			        <p></p>
+			      </div>   
+			    </div>
+			    <div class="carousel-item">
+			      <img src="img/2.png" alt="Chicago" width="1100" height="500">
+			      <div class="carousel-caption">
+			        <h3>¡El exito al alcance de tus manos!</h3>
+			        <p>Thank you, Chicago!</p>
+			      </div>   
+			    </div>
+			    <div class="carousel-item">
+			      <img src="img/3.jpg" alt="New York" width="1100" height="500">
+			      <div class="carousel-caption">
+			        <h3>¡Comprar en línea desde la comodidad de tu hogar!</h3>
+			        <p>We love the Big Apple!</p>
+			      </div>   
+ 			   </div>
+ 			 </div>
+ 			 <a class="carousel-control-prev" href="#demo" data-slide="prev">
+  			  <span class="carousel-control-prev-icon"></span>
+ 			 </a>
+  			<a class="carousel-control-next" href="#demo" data-slide="next">
+   			 <span class="carousel-control-next-icon"></span>
+ 			</a>
+		  </div>
+  		</div>
+  	</div>		
+  </div>
+</div>
+</div>
+			<script src="Estilos/js/jquery.min.js"></script>
+    	<script src="Estilos/js/bootstrap.min.js"></script>
+    	<script src="Estilos/js/scripts.js"></script>
+    	 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>	
+
+</body>
+</html>
