7e63226aff29773972723c58b7a714892c22a987
AYoung27 <adan_19941@hotmail.com>
2019-04-17 15:04:48 -0600

    arreglos(ajax adan)

--- perfil.php
+++ perfil.php
@@ -53,7 +53,7 @@
 	<title>Emprende Fácil</title>	
 </head>
 
-<body onload="cargarDiv('barra', 'Contenido/header.php'),listar(''),cargarDiv('zonaUsuario', 'Contenido/columnaPerfil.php'), cargarDiv('zonaContenido', 'Contenido/informacionDeContacto.php'), $.getScript('Estilos/js/script.js')">
+<body onload="cargarDiv('barra', 'Contenido/header.php'),cargarDiv('zonaUsuario', 'Contenido/columnaPerfil.php'), cargarDiv('zonaContenido', 'Contenido/informacionDeContacto.php'), $.getScript('Estilos/js/script.js')">
 	<!--Incluido en todas las paginas, incluye la barra de navegación y sus botones-->
 	<header>
 		<div id="barra"></div>
@@ -166,16 +166,17 @@
 	<footer>
 		<p class="mt-5 mb-3 text-muted text-center">&copy; 2018-2019</p>	
 	</footer>
+
 	<script src="Estilos/js/jquery.min.js"></script>
 	<script src="Estilos/js/bootstrap.min.js"></script>
 	<script src="http://code.jquery.com/jquery-1.6.3.min.js"></script>
 	<script src="Estilos/js/cargarContenido.js"></script>
 	<script src="Estilos/js/jquery.filedrop.js"></script>
 	<script src="Estilos/js/jquery.script.js"></script>
-	<script type="text/javascript">
+			<script type="text/javascript">
 			function listar(valor){
 				$.ajax({
-				url:'../Acciones/busquedaDatosProducto.php',
+				url:'Acciones/busquedaDatosProducto.php',
 				type:'POST',
 				data:'val='+valor
 				}).done(function(resp){
@@ -186,7 +187,7 @@
 					html+="<tr><td>"+valores[i][0]+"</td><td>"+valores[i][1]+'</td><td><button type='+'"button"'+'class='+'"btn btn-success mr-2"'+" "+'data-toggle='+'"modal"'+" "+' data-target='+'"#modalM"'+'><i class='+'"glyphicon glyphicon-pencil"'+'></i> Modificar</button><button class='+'"btn btn-danger"'+'><i class='+'"glyphicon glyphicon-remove"'+'></i> Eliminar</button></td></tr>';
 				}
 				html+="</tbody></table>";
-				$("#Producto").html(html);
+				$("#divProducto").html(html);
 				});
 			}
 		</script>
