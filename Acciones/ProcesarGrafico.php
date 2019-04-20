143ba1276481a22a4f7cc42505ef278f61da0b83
jairo <jairomauriciogmez@hotmail.com>
2019-04-19 20:21:07 -0600

    act
    cambios en estadistica y precesargrafico

--- Acciones/ProcesarGrafico.php
+++ Acciones/ProcesarGrafico.php
@@ -7,12 +7,14 @@ include("../Clases/Conexion.php");
 
 
 function build_report($year){
+	session_start();
 	$conexion = new Conexion();
 	$conexion->mysql_set_charset("utf8");
+
 	$total = array();
 	for($i=0; $i<12; $i++){
 		$month = $i+1;
-		$consulta="SELECT SUM(monto_venta) AS total FROM ventas WHERE MONTH(fecha_venta) = '$month' AND YEAR(fecha_venta) = '$year' LIMIT 1";
+		$consulta="SELECT SUM(tbl_detalle_factura.Total) AS total FROM tbl_detalle_factura inner join tbl_factura on tbl_detalle_factura.IDFactura=tbl_factura.IDFactura WHERE ". $_SESSION['Proveedor']." =tbl_factura.IDProveedor AND  MONTH(tbl_factura.FechaFactura) = '$month' AND YEAR(tbl_factura.FechaFactura) = '$year' LIMIT 1";
 		$resultado=$conexion->ejecutarconsulta($consulta);	
 		$total[$i] = 0;
 		foreach ($resultado as $key){ $total[$i] = ($key['total'] == null)? 0 : $key['total']; }
--- Contenido/estadistica.php
+++ Contenido/estadistica.php
@@ -10,31 +10,31 @@ if (empty($_SESSION)) {
 
 <div class="col-md-12" style="border-width: 1px 1px 1px 1px; border-style: solid; border-color: lightgray;">
 	<br>
-	<h4 style="text-align: center;">
-		Estadísticas
+	<h4 style="text-align: center; padding-bottom: 1.5em;" >
+		<span>Estadísticas De Ventas</span>
 	</h4>
 	<div class="row">
 		<div class="col-md-12">
 
 			<div class="container">
 				<div class="row">
-					<form class="needs-validation" novalidate>
-						<div class="form-row">
-							<div class="col-md-4 mb-3">
-								<label class="" for="slc_categoria">Seleccione un Año</label>
+					
 
-								<select class="custom-select" id="slc_Ano" onchange="mostrarResultados( this.value,document.getElementById('slcgrafico').value )">
-									<option selected>Año...</option>
-									<?php
-									for($i=2000;$i<2040;$i++){
-										echo '<option value="'.$i.'">'.$i.'</option>';
-									}
-									?>
-								</select>				
-							</div>
+					<div class="col-md-6 mb-3">
+						<label class="" for="slc_categoria">Seleccione un Año</label>
 
-							<div class="col-md-4 mb-3">
-								<label class="" for="slcCategoria">Seleccione un producto</label>
+						<select class="custom-select" id="slc_Ano" onclick="mostrarResultados( this.value,document.getElementById('slcgrafico').value )">
+							<option selected>Año</option>
+							<?php
+							for($i=2012;$i<2040;$i++){
+								echo '<option value="'.$i.'">'.$i.'</option>';
+							}
+							?>
+						</select>				
+					</div>
+
+						<!--		<div class="col-md-4 mb-3">
+								<label class="" for="slcProductoo">Seleccione un producto</label>
 								<?php  
 								$consulta="SELECT IDProducto, NombreProducto FROM tbl_producto";
 								$resultado=$conexion->ejecutarconsulta($consulta);
@@ -47,16 +47,18 @@ if (empty($_SESSION)) {
 									}
 									?>
 								</select>										
-							</div>
+							</div>-->
 
-							<div class="col-md-4 mb-3">
+
+
+							<div class="col-md-6 mb-3 " >
 								<label class="" for="slcGrafico">Seleccione el Tipo de Grafico</label>
 								<?php  
 								$consulta="SELECT IDGrafico, NombreGrafico FROM tbl_graficos";
 								$resultado=$conexion->ejecutarconsulta($consulta);
 								?>
-								<select class="custom-select " name="slcgrafico" id="slcgrafico" onchange="mostrarResultados(document.getElementById('slc_Ano').value , this.value )">
-									<option selected>Grafico...</option>
+								<select class="custom-select " name="slcgrafico" id="slcgrafico" onclick="mostrarResultados(document.getElementById('slc_Ano').value , this.value )">
+									<option selected>Grafico</option>
 									<?php 
 									while ($arreglo=$resultado->fetch_array()) {
 										echo '<option value="'.$arreglo[IDGrafico].'">'.$arreglo[NombreGrafico]."</option>";
@@ -64,18 +66,18 @@ if (empty($_SESSION)) {
 									?>
 								</select>	
 							</div>
+
+
 						</div>
-					</form>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
 
-<div class="row">
+		<div class="row">
 
-	<div class="col-md-12" style="padding-top: 3em;"> <br> <br>
-		<div class="resultados"><canvas id="graficos"></canvas></div>
-	</div>
-</div>	
+			<div class="col-md-12" style="padding-top: 1em;"> <br> <br>
+				<div class="resultados"><canvas id="graficos"></canvas></div>
+			</div>
+		</div>	
 
