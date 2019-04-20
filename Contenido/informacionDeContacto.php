ff865489779bbc932eb7a5295d50ae720b8d0a6c
abn1227 <37668034+abn1227@users.noreply.github.com>
2019-04-20 14:25:06 -0600

    Update informacionDeContacto.php

--- Contenido/informacionDeContacto.php
+++ Contenido/informacionDeContacto.php
@@ -1,15 +1,24 @@
-<div class="col-md-12" style="border-width: 1px 1px 1px 1px; border-style: solid; border-color: lightgray;">
-	<br>
-	<h4 style="text-align: center;">Información de Contacto</h4>
-	<p class="text-center text-muted">Nombre de la Empresa: </p>
-	<p class="text-center text-muted">Correo Electrónico: </p>
-	<p class="text-center text-muted">Teléfono: </p>
-	<p class="text-center text-muted">Dirección: </p>
-	<p class="text-center">Redes Sociales: </p>
-	<div class="col-md-12 text-center">
-		<ul class="list-unstyled list-inline list-social-icons">
-			<a href="https://www.facebook.com">Facebook</a>
-			<a href="#">Twitter</a>
-		</ul>
-	</div>
-</div>
+<?php 
+	session_start();
+	include("../Clases/Conexion.php");
+	$conexion = new Conexion();
+	$conexion->mysql_set_charset("utf8");
+
+	function cargarInformacion($conexion)
+	{
+		$consulta = sprintf("SELECT NombreProveedor, Telefono, Direccion, tbl_municipio.NombreMunicipio, tbl_departamentos.NombreDepartamento FROM tbl_proveedor, tbl_municipio, tbl_departamentos WHERE IDUsuario = '%s' AND tbl_municipio.IDMunicipio = tbl_proveedor.IDMunicipio AND tbl_municipio.IDDepartamento = tbl_departamentos.IDDepartamento", $conexion->antiInyeccion($_SESSION["ID"]));
+		$resultado = $conexion->ejecutarconsulta($consulta);
+		$data = $conexion->obtenerFila($resultado);
+		echo '<div class="col-md-12" style="border-width: 1px 1px 1px 1px; margin-top: 1rem; border-style: solid; border-color: lightgray;">
+					<br>
+					<h4 style="text-align: center;">Información de Contacto</h4>
+					<p class="text-center text-muted">Nombre de la Empresa: '.$data["NombreProveedor"].'</p>
+					<p class="text-center text-muted">Correo Electrónico: '.$_SESSION["Correo"].'</p>
+					<p class="text-center text-muted">Teléfono: '.$data["Telefono"].'</p>
+					<p class="text-center text-muted">Dirección: '.$data["Direccion"].', '.$data["NombreMunicipio"].', '.$data["NombreDepartamento"].'</p>	
+				</div>';
+	}
+	if (isset($_SESSION["Proveedor"])) {
+			cargarInformacion($conexion);
+	}
+ ?>
