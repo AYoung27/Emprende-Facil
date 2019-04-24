5c4387f9d80ce078ed3db6857a868a048c828e27
AYoung27 <adan_19941@hotmail.com>
2019-04-24 02:02:57 -0600

    me falto la descripcion alv

--- detalle.php
+++ detalle.php
@@ -182,7 +182,12 @@ function cargarDetalleProducto($conexion, $idProducto){
                  <p class="mb-5"><h4 style="text-align: center;">Descripcion</h4></p>  
 
                   <div class="row">
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                      <?php
+                        $sql= "SELECT Descripcion FROM tbl_producto where IDProducto=".$_GET['idp'];
+                        $resul=$conexion->ejecutarconsulta($sql);
+                        $descrip=$conexion->obtenerFila($resul);
+                        echo "<p>".$descrip['Descripcion']."</p>";
+                        ?>
                 </div>
                 <hr>
 
